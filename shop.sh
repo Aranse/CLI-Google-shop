1ef4e92c8757506030ca55d04336987752307465
Kaluk <aransekaluk50@gmail.com>
2022-04-20 15:47:15 -0500

    V0.01 not finished

--- shop.sh
+++ shop.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-#rm tmp.html
 string=''
+n1=$'\n'
 for i in "$*"; do string=${string}${i}; done;
 string=${string// /+}
-#webpage=$(curl 'https://www.google.com/search?q='${string}'&source=lnms&tbm=shop' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0')
-#echo ${webpage}| pup "div.sh-pr__product-results-grid sh-pr__product-results"
-curl 'https://www.google.com/search?q='${string}'&source=lnms&tbm=shop' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0'>>tmp.html
+webpage=$(curl 'https://www.google.com/search?q='${string}'&source=lnms&tbm=shop' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0')
+titulos=$(echo $webpage |  pup "h4 text{}") 
+precios=$(echo $webpage | pup ".QIrs8 text{}"|sed 's/[^0-9.,]*//g;')
+links=$(echo $webpage|pup "a.xCpuod attr{href}" | sed 's/url?url=//g'|sed 's/%3F.*/\n\n/g;s/shopp.*/\n\n/g' )
+echo ${titulos}
+echo "#########################################################################################"
+echo ${precios}
+echo "#########################################################################################"
+echo -e $links
